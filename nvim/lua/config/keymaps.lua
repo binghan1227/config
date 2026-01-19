ec04c462ef75fdd962e69fde9c0f5411c0c0f3a0
binghan1227 <binghan1227@126.com>
2026-01-19 18:44:03 +0800

    add the keymap for line movement

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -13,3 +13,10 @@ vim.keymap.set("n", "<leader>y", function()
 	-- After snacks loads, vim.notify already points to Snacks.notifier.notify
 	vim.notify("Yank entire file to +", vim.log.levels.INFO, { timeout = 50, title = "Yank" })
 end, { desc = "Yank entire file to + (clipboard)" })
+
+
+local map = vim.keymap.set
+
+-- Normal: move current line
+map("n", "<C-j>", "<cmd>m .+1<CR>==", { desc = "Move line down" })
+map("n", "<C-k>", "<cmd>m .-2<CR>==", { desc = "Move line up" })
